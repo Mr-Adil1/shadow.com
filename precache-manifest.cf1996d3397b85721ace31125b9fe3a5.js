4064777379232a00916a8e06a7981a6841434982
MuhammadAdil <95621576+Mr-Adil1@users.noreply.github.com>
2022-09-26 09:20:25 -0700

    Updates

---
+++ precache-manifest.cf1996d3397b85721ace31125b9fe3a5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6a4116713c008624cc6",
+    "url": "/shadow.com/static/css/main.e4fd51d0.chunk.css"
+  },
+  {
+    "revision": "e6a4116713c008624cc6",
+    "url": "/shadow.com/static/js/main.e6a41167.chunk.js"
+  },
+  {
+    "revision": "4567e6b4a7bd4ab0b748",
+    "url": "/shadow.com/static/js/1.4567e6b4.chunk.js"
+  },
+  {
+    "revision": "54e133e73594631012e0",
+    "url": "/shadow.com/static/css/2.cf2fa1eb.chunk.css"
+  },
+  {
+    "revision": "54e133e73594631012e0",
+    "url": "/shadow.com/static/js/2.54e133e7.chunk.js"
+  },
+  {
+    "revision": "2c3ad360de34b7e0af0a",
+    "url": "/shadow.com/static/js/runtime~main.2c3ad360.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/shadow.com/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/shadow.com/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/shadow.com/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/shadow.com/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/shadow.com/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "7e736184882ab10dd4452640cc1086e5",
+    "url": "/shadow.com/index.html"
+  }
+];
